9dce33bec0fbebe3262447d03bdcd5bf6bfc22b9
Andrei Esipov <androzeba@gmail.com>
2020-09-22 16:10:43 +0400

    Add exceptions handling with status return

--- src/main/java/ru/internship/voting/util/ValidationUtil.java
+++ src/main/java/ru/internship/voting/util/ValidationUtil.java
@@ -1,8 +1,12 @@
 package ru.internship.voting.util;
 
+import org.slf4j.Logger;
 import ru.internship.voting.model.AbstractBaseEntity;
+import ru.internship.voting.util.exception.ErrorType;
 import ru.internship.voting.util.exception.IllegalRequestDataException;
 import ru.internship.voting.util.exception.NotFoundException;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class ValidationUtil {
 
@@ -14,11 +18,6 @@ public class ValidationUtil {
     public static void checkNotFoundWithId(boolean found, int id) {
         checkNotFound(found, "id=" + id);
     }
-
-//    public static <T> T checkNotFound(T object, String msg) {
-//        checkNotFound(object != null, msg);
-//        return object;
-//    }
 
     public static void checkNotFound(boolean found, String arg) {
         if (!found) {
@@ -38,5 +37,26 @@ public class ValidationUtil {
         } else if (bean.getId() != id) {
             throw new IllegalRequestDataException(bean + " must be with id=" + id);
         }
+    }
+
+    //  http://stackoverflow.com/a/28565320/548473
+    public static Throwable getRootCause(Throwable t) {
+        Throwable result = t;
+        Throwable cause;
+
+        while (null != (cause = result.getCause()) && (result != cause)) {
+            result = cause;
+        }
+        return result;
+    }
+
+    public static Throwable logAndGetRootCause(Logger log, HttpServletRequest req, Exception e, boolean logException, ErrorType errorType) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        if (logException) {
+            log.error(errorType + " at request " + req.getRequestURL(), rootCause);
+        } else {
+            log.warn("{} at request  {}: {}", errorType, req.getRequestURL(), rootCause.toString());
+        }
+        return rootCause;
     }
 }
